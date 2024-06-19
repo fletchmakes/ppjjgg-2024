82bbc30ef9d82f626e00bedda26fd919ac65c07a
Fletch <fletchmakesstuff@gmail.com>
2024-06-19 16:08:50 -0400

    Updated theme with button textures; added main menu

---
+++ scripts/MainMenu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var display_state = "shown";
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (self.display_state == "hiding"):
+		var current_modulate: Color = self.get_modulate();
+		var current_alpha = current_modulate.a;
+		var next_alpha = lerp(current_alpha, 0.0, 0.1);
+		if (next_alpha - 0 < 0.05):
+			self.queue_free();
+		else:
+			self.set_modulate(Color(current_modulate.r, current_modulate.g, current_modulate.b, next_alpha));
+			
+
+func _on_play_button_pressed():
+	self.display_state = "hiding";
