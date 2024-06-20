bee03276ddcb1995dc83967a88d6a89abdfaa553
Fletch <fletchmakesstuff@gmail.com>
2024-06-20 14:05:52 -0400

    yawns added teehee

--- classes/AllTheText.gd
+++ classes/AllTheText.gd
@@ -4,26 +4,25 @@ class_name AllTheText
 # PUT ALL NEW TEXT AT THE BOTTOM OF THE RESOURCES ARRAY
 # PUTTING IT NEAR THE TOP MIGHT RUIN TEXT FROM OTHER PARTS OF THE GAME
 static var RESOURCES: PackedStringArray = [
-	# prologue
-	"once upon a time",#0
-	"there was a young woman",#1
+	# yawn
+	"Wait, what was I doing again?", #0
 	
 	# bed
-	"eepy time ya ding dong",#2
+	"eepy time ya ding dong", #2
 	
 	# epilogue
-	"she lived happily ever after",#3
+	"she lived happily ever after", #3
 	
 	#interactable test
-	"yahaha! you found me!! :D this is really long text to try and see what happens if the text is really long",#4
+	"yahaha! you found me!! :D this is really long text to try and see what happens if the text is really long", #4
 	
 	#object 1
-	"LOOK AT ME I'M ON THE WALL :O",#5
+	"LOOK AT ME I'M ON THE WALL :O", #5
 	
 	#object 2
-	"hello I am another object as you can see",#6
+	"hello I am another object as you can see", #6
 	
 	#object 3
-	"*COOL LORE DROP* OMG!!!!"#7
+	"*COOL LORE DROP* OMG!!!!" #7
 	
 ]
